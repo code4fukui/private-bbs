54a0b898ee7e07baddf430722f6709ebd342a754
Taisuke Fukuno <fukuno@jig.jp>
2025-08-09 09:10:46 +0900

    added log function

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import { makeFetch } from "https://code4fukui.github.io/PubkeyUser/serverutil.js";
-import { TAI64N } from "https://code4fukui.github.io/TAI64N-es/TAI64N.js";
 import { Posts } from "./Posts.js";
+import { JSONLWriter } from "https://code4fukui.github.io/JSONL/JSONLWriter.js";
+import { DateTime, TimeZone } from "https://js.sabae.cc/DateTime.js";
 
 const posts = await Posts.create();
 
-const api = async (path, param, pubkey) => {
-  console.log("api", path, path == "add", param, pubkey)
+const logdir = "log";
+
+const log = async (pubkey, path, param, req, conn) => {
+  const ua = req.headers.get("user-agent");
+  const dt = new DateTime();
+  const ymd = dt.toLocal(TimeZone.JST).day.toStringYMD();
+  const w = new JSONLWriter(logdir + "/" + ymd + ".jsonl", true);
+  await w.writeRecord({ pubkey, path, param, ua });
+  w.close();
+};
+
+const api = async (path, param, pubkey, req, conn) => {
+  //console.log("api", path, path == "add", param, pubkey)
+  log(pubkey, path, param, req, conn);
   if (!pubkey) return "no pubkey";
   if (path == "add") {
     console.log("add", path)
