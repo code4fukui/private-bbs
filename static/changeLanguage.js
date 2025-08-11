8d355812b5eae0245574608b369d71ebd168366c
Taisuke Fukuno <fukuno@jig.jp>
2025-08-11 15:46:16 +0900

    tab to space

--- static/changeLanguage.js
+++ static/changeLanguage.js
@@ -6,45 +6,45 @@ let flginit = false;
 let data = null;
 
 const initTranslation = async () => {
-	if (flginit) return;
-	flginit = true;
+  if (flginit) return;
+  flginit = true;
   // ja,mn,en
-	data = await CSV.fetchJSON("translation.csv");
-	
-	const txts = document.querySelectorAll(".text");
-	txts.forEach(i => {
-		i.flgplaceholder = !!i.placeholder;
-		const ja = i.textContent || i.placeholder;
-		if (!ja) return;
-		const item = data.find(i => i.ja == ja);
-		if (item) {
-			i.translations = item;
-		}
-	});
+  data = await CSV.fetchJSON("translation.csv");
+  
+  const txts = document.querySelectorAll(".text");
+  txts.forEach(i => {
+    i.flgplaceholder = !!i.placeholder;
+    const ja = i.textContent || i.placeholder;
+    if (!ja) return;
+    const item = data.find(i => i.ja == ja);
+    if (item) {
+      i.translations = item;
+    }
+  });
 };
 
 export const translate = (ja, lang) => {
-	const item = data.find(i => i.ja == ja);
-	if (!item || !item[lang]) return ja;
-	return item[lang];
+  const item = data.find(i => i.ja == ja);
+  if (!item || !item[lang]) return ja;
+  return item[lang];
 };
 
 export const changeLanguage = async (lang = null) => {
-	if (!flginit) {
-		await initTranslation();
-	}
+  if (!flginit) {
+    await initTranslation();
+  }
   if (!lang) {
     lang = getLanguage();
   }
-	const txts = document.querySelectorAll(".text");
-	txts.forEach(i => {
-		if (!i.translations) return;
-		const txt = i.translations[lang];
-		if (!txt) return;
-		if (i.flgplaceholder) {
-			i.placeholder = txt;
-		} else {
-			i.innerHTML = makeTextWithLink(txt);
-		}
-	});
+  const txts = document.querySelectorAll(".text");
+  txts.forEach(i => {
+    if (!i.translations) return;
+    const txt = i.translations[lang];
+    if (!txt) return;
+    if (i.flgplaceholder) {
+      i.placeholder = txt;
+    } else {
+      i.innerHTML = makeTextWithLink(txt);
+    }
+  });
 };
